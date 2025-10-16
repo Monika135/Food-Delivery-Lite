bb6a73171680c4fab581a76aa751ea37c738c68e
Monika <85540663+Monika135@users.noreply.github.com>
2025-10-16 19:22:13 +0530

    Refactor ASGI configuration for Django channels

--- food_delivery/asgi.py
+++ food_delivery/asgi.py
@@ -25,23 +25,23 @@
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from bookings.routing import websocket_urlpatterns
+from bookings.middleware import JWTAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_delivery.settings")
 
+# Setup Django before importing anything that uses models
 django.setup()
 
-
-from bookings.routing import websocket_urlpatterns
-from bookings.middleware import JWTAuthMiddleware  # if you have a custom middleware
-
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
-    "http": django.core.asgi.get_asgi_application(),
+    "http": get_asgi_application(),
     "websocket": JWTAuthMiddleware(
         URLRouter(websocket_urlpatterns)
     ),
 })
+
 
 
 
