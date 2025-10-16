2aa7223e34a1e3be1d71320ee9f84dcc8c722c99
Monika <11613.mona@gmail.com>
2025-10-16 18:59:57 +0530

    Your descriptive commit message

--- bookings/consumers.py
+++ bookings/consumers.py
@@ -1,59 +1,59 @@
-# import json
-# from channels.generic.websocket import AsyncWebsocketConsumer
-# from channels.db import database_sync_to_async
-# from .models import Booking, ChatMessage
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Booking, ChatMessage
 
-# class ChatConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         self.booking_id = self.scope['url_route']['kwargs']['booking_id']
-#         print("WebSocket trying to connect with user:", self.scope['user'])
-#         self.user = self.scope['user']
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.booking_id = self.scope['url_route']['kwargs']['booking_id']
+        print("WebSocket trying to connect with user:", self.scope['user'])
+        self.user = self.scope['user']
 
-#         # Access check: only customer or partner
-#         if not await self.check_booking_access(self.booking_id, self.user):
-#             await self.close()
-#             return
+        # Access check: only customer or partner
+        if not await self.check_booking_access(self.booking_id, self.user):
+            await self.close()
+            return
 
-#         self.room_group_name = f'chat_{self.booking_id}'
-#         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-#         await self.accept()
+        self.room_group_name = f'chat_{self.booking_id}'
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
-#     async def disconnect(self, close_code):
-#         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-#     async def receive(self, text_data):
-#         data = json.loads(text_data)
-#         message = data.get('message', '')
-#         if not message:
-#             return
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get('message', '')
+        if not message:
+            return
 
-#         await self.save_message(self.booking_id, self.user, message)
+        await self.save_message(self.booking_id, self.user, message)
 
-#         # Broadcast to group
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 'type': 'chat_message',
-#                 'message': message,
-#                 'user': self.user.username
-#             }
-#         )
+        # Broadcast to group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username
+            }
+        )
 
-#     async def chat_message(self, event):
-#         await self.send(text_data=json.dumps({
-#             'message': event['message'],
-#             'user': event['user']
-#         }))
+    async def chat_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': event['message'],
+            'user': event['user']
+        }))
 
-#     @database_sync_to_async
-#     def check_booking_access(self, booking_id, user):
-#         try:
-#             booking = Booking.objects.get(id=booking_id)
-#             return user == booking.customer or user == booking.partner
-#         except Booking.DoesNotExist:
-#             return False
+    @database_sync_to_async
+    def check_booking_access(self, booking_id, user):
+        try:
+            booking = Booking.objects.get(id=booking_id)
+            return user == booking.customer or user == booking.partner
+        except Booking.DoesNotExist:
+            return False
 
-#     @database_sync_to_async
-#     def save_message(self, booking_id, user, message):
-#         booking = Booking.objects.get(id=booking_id)
-#         return ChatMessage.objects.create(booking=booking, sender=user, message=message)
+    @database_sync_to_async
+    def save_message(self, booking_id, user, message):
+        booking = Booking.objects.get(id=booking_id)
+        return ChatMessage.objects.create(booking=booking, sender=user, message=message)
--- food_delivery/asgi.py
+++ food_delivery/asgi.py
@@ -1,39 +1,39 @@
-# import os
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
-
-# # Safe to import routing here
-# import bookings.routing
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": None,  # placeholder, will set below
-# })
-
-
-# from bookings.middleware import JWTAuthMiddleware
-
-# application["websocket"] = JWTAuthMiddleware(
-#     URLRouter(
-#         bookings.routing.websocket_urlpatterns
-#     )
-# )
-
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
 
-application = get_asgi_application()
+# Safe to import routing here
+import bookings.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": None,  # placeholder, will set below
+})
+
+
+from bookings.middleware import JWTAuthMiddleware
+
+application["websocket"] = JWTAuthMiddleware(
+    URLRouter(
+        bookings.routing.websocket_urlpatterns
+    )
+)
+
+# """
+# ASGI config for mysite project.
+
+# It exposes the ASGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+# """
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# application = get_asgi_application()
