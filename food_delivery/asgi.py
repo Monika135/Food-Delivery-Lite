5dfdc1689a521c8563848db74c274499888f62ba
Monika <85540663+Monika135@users.noreply.github.com>
2025-10-16 19:12:14 +0530

    Refactor ASGI configuration for WebSocket support

--- food_delivery/asgi.py
+++ food_delivery/asgi.py
@@ -1,26 +1,44 @@
+# import os
+# import django
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
+# django.setup()
+# # Safe to import routing here
+# import bookings.routing
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": None,  # placeholder, will set below
+# })
+
+
+# from bookings.middleware import JWTAuthMiddleware
+
+# application["websocket"] = JWTAuthMiddleware(
+#     URLRouter(
+#         bookings.routing.websocket_urlpatterns
+#     )
+# )
+
 import os
 import django
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from bookings.routing import websocket_urlpatterns
+from bookings.middleware import JWTAuthMiddleware  # your custom middleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "food_delivery.settings")
 django.setup()
-# Safe to import routing here
-import bookings.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": None,  # placeholder, will set below
+    "websocket": JWTAuthMiddleware(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
 
-
-from bookings.middleware import JWTAuthMiddleware
-
-application["websocket"] = JWTAuthMiddleware(
-    URLRouter(
-        bookings.routing.websocket_urlpatterns
-    )
-)
 
 # """
 # ASGI config for mysite project.
