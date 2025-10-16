ccf6af888f5fe9e554f061a7d4ccb2edebbb158b
Monika <85540663+Monika135@users.noreply.github.com>
2025-10-16 16:54:00 +0530

    Refactor ASGI application to set websocket routing

--- food_delivery/asgi.py
+++ food_delivery/asgi.py
@@ -1,16 +1,22 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from bookings.middleware import JWTAuthMiddleware
-import bookings.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food_delivery.settings')
 
+# Safe to import routing here
+import bookings.routing
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        URLRouter(
-            bookings.routing.websocket_urlpatterns
-        )
-    ),
+    "websocket": None,  # placeholder, will set below
 })
+
+
+from bookings.middleware import JWTAuthMiddleware
+
+application["websocket"] = JWTAuthMiddleware(
+    URLRouter(
+        bookings.routing.websocket_urlpatterns
+    )
+)
